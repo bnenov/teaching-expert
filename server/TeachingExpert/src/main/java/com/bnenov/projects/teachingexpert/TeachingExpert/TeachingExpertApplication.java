3f6f9b86549f0ab1f8b3dfa669f9cc1572e555b9
Borislav Nenov <borislav.nenov.93@gmail.com>
2022-01-17 12:46:40 +0200

    Initial project created

---
+++ server/TeachingExpert/src/main/java/com/bnenov/projects/teachingexpert/TeachingExpert/TeachingExpertApplication.java
@@ -0,0 +1,13 @@
+package com.bnenov.projects.teachingexpert.TeachingExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingExpertApplication.class, args);
+	}
+
+}
---
+++ server/TeachingExpert/src/test/java/com/bnenov/projects/teachingexpert/TeachingExpert/TeachingExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnenov.projects.teachingexpert.TeachingExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
